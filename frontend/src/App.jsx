588ee3111137f65168ede0e20f4b4331985f8998
noelking024 <noelkandlapalli24@gmail.com>
2025-09-24 11:17:10 +0530

    Add main.jsx and App.jsx and define component routings

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import { getToken } from './services/api';
+import './global.css'
+
+function PrivateRoute({ children }) {
+  const token = getToken();
+  return token ? children : <Navigate to="/login" replace />;
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
